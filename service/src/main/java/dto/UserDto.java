833d17fd96ba4d3da4aa8294c289859714484859
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2022-11-25 15:19:42 +0300

    DTO transformed to a record

--- service/src/main/java/dto/UserDto.java
+++ service/src/main/java/dto/UserDto.java
@@ -1,37 +1,28 @@
 package dto;
 
 import entities.RoleName;
-import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 
-@Data
-public class UserDto implements Serializable {
+public record UserDto(
+        @NotBlank(message = "The field must not be empty")
+        @Size(min = 1, max = 40, message = "Up to 40 latin letters allowed")
+        @Pattern(regexp = "[a-zA-Z]*", message = "Only latin letters are allowed")
+        String firstName,
+        @NotBlank(message = "The field must not be empty")
+        @Size(min = 1, max = 20, message = "Up to 20 latin letters allowed")
+        @Pattern(regexp = "[a-zA-Z]*", message = "Only latin letters are allowed") String secondName,
+        @NotBlank(message = "The field must not be empty")
+        @Size(min = 1, max = 40, message = "Up to 40 latin letters allowed")
+        @Pattern(regexp = "[a-zA-Z]*", message = "Only latin letters are allowed") String surname,
+        @Email(message = "Must match email format")
+        @Size(max = 50, message = "Up to 50 latin characters allowed")
+        String email,
+        Integer roleId,
+        RoleName roleName) implements Serializable {
 
-    @NotBlank(message = "Поле не должно быть пустым")
-    @Size(min = 1, max = 40, message = "Допускается до 40 латинских букв")
-    @Pattern(regexp = "[a-zA-Z]*", message = "Допустимы только латинские буквы")
-    private final String firstName;
-
-    @NotBlank(message = "Поле не должно быть пустым")
-    @Size(min = 1, max = 20, message = "Допускается до 20 латинских букв")
-    @Pattern(regexp = "[a-zA-Z]*", message = "Допустимы только латинские буквы")
-    private final String secondName;
-
-    @NotBlank(message = "Поле не должно быть пустым")
-    @Size(min = 1, max = 40, message = "Допускается до 40 латинских букв")
-    @Pattern(regexp = "[a-zA-Z]*", message = "Допустимы только латинские буквы")
-    private final String surname;
-
-    @Email(message = "Должно соответствовать формату email")
-    @Size(max = 50, message = "Допускается до 50 латинских символов")
-    private final String email;
-
-    private final Integer roleId;
-
-    private final RoleName roleName;
 }
